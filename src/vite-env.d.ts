583307b13c64e86a2499754145a9ed02d0b2df8e
Boe <2066527398@qq.com>
2026-04-01 16:38:57 +0800

    :

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,12 @@ declare module '*.vue' {
   export default component
 }
 
+// 1. 解决 main.ts 中 zh-cn.mjs 标红的问题
+declare module 'element-plus/dist/locale/zh-cn.mjs' {
+  const content: any
+  export default content
+}
+
 declare module 'virtual:uno.css' {
   const css: string
   export default css
